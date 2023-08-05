3d2cd3c136e7de5c789fdca510e0700420cfddd4
skeender <138387306+skeender@users.noreply.github.com>
2023-08-05 09:27:32 -0400

    Create setup.py

---
+++ saconvert/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='saconvert',
+    version='0.1.1',
+    description="saconvert is a versatile Python library focused on length conversions, providing a seamless interface to effortlessly convert between various units like meters, feet, inches, and more. With a growing list of upcoming conversions, saconvert simplifies complex length transformations, making it a must-have tool for developers and engineers working with measurements. Empower your projects with the efficiency and precision of saconvert, and stay tuned for new exciting conversion features to be added soon!",
+    author='Skanda Acharya',
+    author_email='skanda.acharya@yahoo.com',
+    url='https://github.com/skeender/saconvert',
+    packages=['saconvert'],
+    install_requires=[],
+)
